0b2f84040a5ddcabab93f7f20f668a30c67e8c09
felixkellenbenz <fe.kellenbenz.computer@outlook.de>
2025-07-08 22:17:34 +0200

    add C autocompletion

--- emacs/init.el
+++ emacs/init.el
@@ -2,6 +2,11 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
+(require 'package)
+(add-to-list 'package-archives '(melpa . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,7 +15,7 @@
  '(custom-safe-themes
    '("fec5c912ae88d062ef2225f2552641759e9fa4c221a25f5210aae1fcc52f7c6f"
      default))
- '(package-selected-packages '(##)))
+ '(package-selected-packages '(## company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
